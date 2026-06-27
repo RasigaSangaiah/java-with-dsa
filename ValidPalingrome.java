7b2c9c1fe14d13ee6294f3907471472244aca0f2
RasigaSangaiah <rasiga093@gmail.com>
2026-06-27 11:41:04 +0530

    Add files via upload

---
+++ ValidPalingrome.java
@@ -0,0 +1,52 @@
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+
+        String str = "madbam";
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) == str.charAt(right)) {
+                left++;
+                right--;
+            } else {
+
+                // Remove left character
+                String s1 = str.substring(left + 1, right + 1);
+
+                // Remove right character
+                String s2 = str.substring(left, right);
+
+                if (checkPalindrome(s1) || checkPalindrome(s2)) {
+                    System.out.println(true);
+                } else {
+                    System.out.println(false);
+                }
+                return;
+            }
+        }
+
+        System.out.println(true);
+    }
+
+    public static boolean checkPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
