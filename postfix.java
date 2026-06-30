084785eea7a0637bf9e4da6719fd4f6e9a1762ea
RasigaSangaiah <rasiga093@gmail.com>
2026-06-30 14:03:26 +0530

    Add files via upload

---
+++ postfix.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class postfix {
+    public static void main(String[] args) {
+
+        String exp = "23*54*+9-";
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0');
+            } 
+            else {
+                int b = stack.pop();
+                int a = stack.pop();
+
+               if(ch=='+')
+                        stack.push(a + b);
+                        
+                else if(ch=='-')
+                        stack.push(a - b);
+                        
+                else if(ch=='*')
+                        stack.push(a * b);
+                        
+                else if(ch=='/')
+                        stack.push(a / b);
+                        
+                
+            }
+        }
+
+        System.out.println("Result = " + stack.pop());
+    }
+}
