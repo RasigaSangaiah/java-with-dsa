e18fb843794df992a7da75e52d8b5eaf940112c2
RasigaSangaiah <rasiga093@gmail.com>
2026-07-02 09:59:41 +0530

    Add files via upload

---
+++ duplicate2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class duplicate2 {
+    public static void main(String[] args) {
+       Set<Integer> set =new HashSet<>();
+       set.add(10);
+       set.add(20);
+       set.add(39);
+       set.add(10);
+       set.add(30);
+       set.add(30);
+       System.out.println(set);
+
+      
+    }
+}
